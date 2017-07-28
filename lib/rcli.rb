97020505d23d85bf0af45df10936f1d3cbd790c7
SengMing Tan <tansengming@gmail.com>
2017-07-28 18:09:23 -0400

    moves all requires to the lib file

--- lib/rcli.rb
+++ lib/rcli.rb
@@ -1,6 +1,12 @@
+require 'uri'
+require 'cgi'
+require 'open-uri'
+require 'json'
 require 'pathname'
+require 'net/http'
 require File.dirname(__FILE__) + "/rcli/config"
 require File.dirname(__FILE__) + "/rcli/profile_formatter"  
+# require 'irb' # debug with binding.irb
 
 module RCLI
 end
