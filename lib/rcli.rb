181c58c85d7386fb0c4bb071850d2ebf22bb2db4
SengMing Tan <tansengming@gmail.com>
2017-08-01 09:58:50 -0400

    do not show password on cli

--- lib/rcli.rb
+++ lib/rcli.rb
@@ -4,6 +4,7 @@ require 'open-uri'
 require 'json'
 require 'pathname'
 require 'net/http'
+require 'io/console'
 require File.dirname(__FILE__) + "/rcli/config"
 require File.dirname(__FILE__) + "/rcli/profile_formatter"  
 # require 'irb' # debug with binding.irb
